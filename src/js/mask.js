75c0ef73f8c099891e8ae6edfa2b5ac5cf0e9f43
Vitaliy-Danilchik <danilchikvvv@gmail.com>
2022-08-05 16:47:05 +0300

    HL

--- src/js/mask.js
+++ src/js/mask.js
@@ -1,7 +1,8 @@
 // Скрипт для форми телефону
-console.log(IMask);
-const phoneMask = IMask(document.getElementById('user-phone'), {
-  mask: '+{38}(\\000)000 00 00',
-  // lazy: false,  // make placeholder always visible
-  placeholderChar: ' ', // defaults to '_'
+console.log(IMask)
+const phoneMask = IMask(
+	document.getElementById('user-phone'), {
+	mask: '+{38}(\\000)000 00 00',
+	lazy: false,  // make placeholder always visible
+	placeholderChar: ' '     // defaults to '_'
 });
